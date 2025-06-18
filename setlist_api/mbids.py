afa3db6fdfb61f4fe630cfa2ef1bd76057a0ab11
Sondre Bolland <sondre.bolland@uib.no>
2025-06-18 23:16:41 +0200

    More mbids

--- setlist_api/mbids.py
+++ setlist_api/mbids.py
@@ -1,5 +1,11 @@
 artist_mbid = {
     "Muse": "9c9f1380-2516-4fc9-a3e6-f9f61941d090",
     "Avenged Sevenfold": "24e1b53c-3085-4581-8472-0b0088d2508c",
-    "Green Day": "084308bd-1654-436f-ba03-df6697104e19"
+    "Green Day": "084308bd-1654-436f-ba03-df6697104e19",
+    "Kaizers Orchestra": "2c3bac0a-5158-442f-b50b-056aae52138c",
+    "Dimmu Borgir": "17167af8-c1da-45cc-bba2-9d23f068b7a3",
+    "Dream Theater": "28503ab7-8bf2-4666-a7bd-2644bfc7cb1d",
+    "Megadeth": "a9044915-8be3-4c7e-b11f-9e2d2ea0a91e",
+    "PLACEHOLDER": "PLACEHOLDER",
+    "PLACEHOLDER": "PLACEHOLDER",
 }
